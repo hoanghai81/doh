921daf43c59afe3f3ca9d4030526644787810f10
hoanghai81 <102153932+hoanghai81@users.noreply.github.com>
2025-11-22 10:59:20 +0700

    Change upstream DoH server to Google DNS

--- deno/doh.ts
+++ deno/doh.ts
@@ -1,6 +1,6 @@
 import { ensureBlocklistsLoaded, isBlocked } from "./blocklist.ts";
 
-const UPSTREAM_DOH = "https://1.1.1.1/dns-query";
+const UPSTREAM_DOH = "https://dns.google/dns-query";
 
 export async function handleDnsJson(request: Request) {
   await ensureBlocklistsLoaded();
